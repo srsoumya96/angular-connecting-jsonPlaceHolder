68e7d32975c4b9f8180b4481fd267c5131f964cd
srsoumya96 <sr.soumya96@gmail.com>
2020-02-06 17:05:39 +0530

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CustomModule } from './custom/custom.module';
 import { FirstComponent } from './first/first.component';
-import { AddCmpComponent } from './add-cmp/add-cmp.component';
 import { RestService } from './rest.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, CustomModule, HttpClientModule ],
-  declarations: [ AppComponent, FirstComponent, AddCmpComponent ],
+  declarations: [ AppComponent, FirstComponent ],
   bootstrap:    [ AppComponent ],
   providers: [RestService]
 })
